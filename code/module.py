372f309a2a868f0c54d89dbba3f2faed76802b7d
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2023-02-23 09:51:20 +0100

    adds workflows and unittests

---
+++ code/module.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def the_function():
+    return np.random.uniform()
---
+++ tests/test_code.py
@@ -0,0 +1,5 @@
+
+def test_the_function():
+    from code.module import the_function
+    a = the_function()
+    assert isinstance(a, float)
